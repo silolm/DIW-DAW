01f25dd1a93831334dbd8d7afb722dcefb6429ed
Mario Silla Olmos <silolm.mail@gmail.com>
2019-10-16 20:27:07 +0200

    Arreglo del mapa -> TODO es Matriz funciones de movimiento funcion de validar travesia

--- primeraEvaluacion/UD02/tarea_OhMummy_EP01/app.js
+++ primeraEvaluacion/UD02/tarea_OhMummy_EP01/app.js
@@ -1,28 +1,90 @@
-window.addEventListener('load', () => {
+let mapa = [];
+let personaje = {};
+personaje.y = 0;
+personaje.x = 8;
 
-    let mapa = [];
+function crearMapa() {
+    let juego = document.querySelector('.juego');
 
-    function crearMapa() {
-        let juego = document.querySelector('.juego');
+    mapa[0] = [];
 
-        for (let i = 0; i < 13; i++) {
-            mapa[i] = [];
-            for (let j = 0; j < 21; j++) {
-                let div = document.createElement('div');
 
-                if (i % 3 === 0 || i === 0 || j === 0 || j % 4 === 0)
-                    div.classList.add('pasillo');
-                else
-                    div.classList.add('celda');
+    for (let z = 0; z < 21; z++) {
+        let div = document.createElement('div');
 
-                mapa[i][j] = div;
-                juego.appendChild(div);
-            }
+        if (z !== 8)
+            div.classList.add('body');
+        else {
+            div.classList.add('pasillo');
+            div.classList.add('personaje');
         }
+
+        // mapa[z] = div;
+        mapa[0][z] = div;
+        juego.appendChild(div);
     }
 
-    // i === [1, 5, 9, 13, 17, 21] || j === [0, 4, 8, 12, 16, 20]
+    for (let i = 1; i < 14; i++) {
+        mapa[i] = [];
+        for (let j = 0; j < 21; j++) {
+            let div = document.createElement('div');
 
-    crearMapa();
+            if (i === 1 || i % 3 === 1 || j % 4 === 0 || j === 0)
+                div.classList.add('pasillo');
+            else
+                div.classList.add('celda');
 
+            let x = div.dataset.x = i;
+            let y = div.dataset.y = j;
+
+            mapa[i][j] = div;
+            juego.appendChild(div);
+        }
+    }
+}
+
+crearMapa();
+
+console.log(mapa);
+
+function moverAbajo() {
+
+    let posY = personaje.y + 1;
+    let posX = personaje.x;
+
+    if (isValid(posY, posX)) {
+        // Eliminar anterior posicion
+        mapa[personaje.y][personaje.x].classList.remove('personaje');
+        // Renovar
+        personaje.x = posX;
+        personaje.y = posY;
+        mapa[personaje.y][personaje.x].classList.add('personaje');
+    }
+
+
+}
+
+function isValid(posY, posX) {
+
+}
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === 'ArrowDown')
+        moverAbajo();
+
+    if (event.key === 'ArrowUp')
+        moverArriba();
+
+    if (event.key === 'ArrowRight')
+        moverDerecha();
+
+    if (event.key === 'ArrowLeft')
+        moverIzquierda();
 });
+
+
+
+
+
+
+
