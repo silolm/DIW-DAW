aaebca6ef709b3a465b9458767e3801c552a9123
Mario Silla Olmos <silolm.mail@gmail.com>
2019-10-23 00:29:39 +0200

    cambiar cerebro por uno NUEVO, almacen de array para realizar consultas de forma que sea mas optimo el codigo, soy un flipao para lo malo q soy pero XE! tirali va

--- primeraEvaluacion/UD02/tarea_OhMummy_EP01/app.js
+++ primeraEvaluacion/UD02/tarea_OhMummy_EP01/app.js
@@ -1,4 +1,5 @@
 let mapa = [];
+let celdas = [];
 let personaje = {};
 personaje.y = 0;
 personaje.x = 8;
@@ -7,7 +8,6 @@ function crearMapa() {
     let juego = document.querySelector('.juego');
 
     mapa[0] = [];
-
 
     for (let z = 0; z < 21; z++) {
         let div = document.createElement('div');
@@ -34,79 +34,132 @@ function crearMapa() {
                 div.classList.add('celda');
 
             // if (i === 1 && j === 6)
-            //     div.classList.add('enemigo');
+            //   div.classList.add('enemigo');
 
             mapa[i][j] = div;
             juego.appendChild(div);
         }
     }
+
+    //inicializamos celdas
+    for (let i = 0; i <= 19; i++)
+        celdas[i] = [(i * 4) + 1];
+
+    //  conseguir la esquina izquierda-arriba de cada caja y crear un ARRAY que las almacene de forma que por Ej.: la CELDA[6] es la primera caja
+    //  de la segunda fila y las cordenadas que contendria seria: [0,4]
+
+
 }
 
 crearMapa();
 
-
-console.log(mapa);
-
-function marcarMuro() {
-
+function marcarCeldas() {
     let posY = personaje.y;
     let posX = personaje.x;
 
     if (posY <= 12 && mapa[posY + 1][posX].className.indexOf('celda') >= 0) {
         mapa[posY + 1][posX].classList.add('X');
-        desbloquearCelda(posY + 1, posX);
+        comprobarCeldas(posY + 1, posX);
     }
     if (posY > 0 && mapa[posY - 1][posX].className.indexOf('celda') >= 0) {
         mapa[posY - 1][posX].classList.add('X');
-        desbloquearCelda(posY - 1, posX);
+        comprobarCeldas(posY - 1, posX);
     }
     if (posX <= 19 && mapa[posY][posX + 1].className.indexOf('celda') >= 0) {
         mapa[posY][posX + 1].classList.add('X');
-        desbloquearCelda(posY, posX + 1);
+        comprobarCeldas(posY, posX + 1);
     }
     if (posX > 0 && mapa[posY][posX - 1].className.indexOf('celda') >= 0) {
         mapa[posY][posX - 1].classList.add('X');
-        desbloquearCelda(posY, posX - 1);
+        comprobarCeldas(posY, posX - 1);
     }
 }
 
-function desbloquearCelda(y, x) {
+function comprobarCeldas(y, x) {
+    //Pasamos la posición del muro
+    let posX = x;
+    let posY = y;
+    let contador = 0;
+    let fuera = false;
 
+    for (let i = 0; i < 6 && !fuera; i++) {
+        //**********************************LATERALES************************************
+
+        //izquierdo
+        if (mapa[y][x - 1].classList.contains('celda') && mapa[y][x - 1].classList.contains('X')) {
+            x--;
+            contador++;
+        }
+        //derecho
+        if (mapa[y][x + 1].classList.contains('celda') && mapa[y][x + 1].classList.contains('X')) {
+            x++;
+            contador++;
+        }
+        //arriba
+        if (mapa[y - 1][x].classList.contains('celda') && mapa[y - 1][x].classList.contains('X')) {
+            y--;
+            contador++;
+        }
+        //abajo
+        if (mapa[y + 1][x].classList.contains('celda') && mapa[y + 1][x].classList.contains('X')) {
+            y++;
+            contador++;
+        }
+
+        //**********************************ESQUINAS**********************************
+
+        // arriba-izquierda
+        if (mapa[y - 1][x - 1].classList.contains('celda') && mapa[y - 1][x - 1].classList.contains('X')) {
+            y++;
+            contador++;
+        }
+    }
+    if (contador === 6)
+        desbloquearCelda(posY, posX);
+
+}
+
+function desbloquearCelda(y, x) {
+    alert("Me corro en tu boca");
 }
 
 function moverAbajo() {
     let posY = personaje.y + 1;
     let posX = personaje.x;
 
-    mover(posY, posX);
+    if (posY <= 13 && esValido(posY, posX))
+        mover(posY, posX);
 }
 
 function moverArriba() {
     let posY = personaje.y - 1;
     let posX = personaje.x;
 
-    mover(posY, posX);
+    if (posY >= 0 && esValido(posY, posX))
+        mover(posY, posX);
 }
 
 function moverDerecha() {
     let posY = personaje.y;
     let posX = personaje.x + 1;
 
-    mover(posY, posX);
+    if (posX <= 20 && esValido(posY, posX))
+        mover(posY, posX);
+
 }
 
 function moverIzquierda() {
     let posY = personaje.y;
     let posX = personaje.x - 1;
 
-    mover(posY, posX);
+    if (posX >= 0 && esValido(posY, posX))
+        mover(posY, posX);
 }
 
 function mover(posY, posX) {
     console.log(posX);
     console.log(posY);
 
-    if (!esValido(posY, posX)) return;
     // Eliminar anterior posicion
     mapa[personaje.y][personaje.x].classList.remove('personaje');
     // Renovar
@@ -114,12 +167,11 @@ function mover(posY, posX) {
     personaje.y = posY;
     mapa[personaje.y][personaje.x].classList.add('personaje');
 
-    marcarMuro();
-
+    marcarCeldas();
 }
 
-function esValido(posY, posX) {
-    return posX >= 0 && posX <= 20 && posY >= 0 && posY <= 13 && mapa[posY][posX].classList.contains("pasillo");
+function esValido(y, x) {
+    return mapa[y][x].classList.contains("pasillo");
 }
 
 window.addEventListener('keydown', (event) => {
@@ -135,3 +187,26 @@ window.addEventListener('keydown', (event) => {
     if (event.key === 'ArrowLeft')
         moverIzquierda();
 });
+
+function llamada() {
+    let posX = personaje.x;
+    let posY = personaje.y;
+
+    if ((posX === 0 || posX === 20 || posX % 4 === 0) && (posY === 1 || posY === 13 || (posY - 1) % 3 === 0)) {
+        if ((posX === 0 || posX === 20) && (posY === 1 || posY === 13)) {
+            // Esquinas del mapa
+        } else {
+            if (posX === 0 || posX === 20) {
+                // intersecciones de tres caminos en los bordes de x
+            } else if (posY === 1 || posY === 13) {
+                // intersecciones de tres caminos en los bordes de y
+            } else {
+                //intersecciones de cuatro caminos
+            }
+        }
+    } else if ((posX === 0 || posX === 20) && (posY === 1 || posY === 13)) {
+        // esquinas del mapa en x e y sin itersecciones
+    } else if (posY !== 0) {
+        //centro mapa sin intersecciones
+    }
+}
