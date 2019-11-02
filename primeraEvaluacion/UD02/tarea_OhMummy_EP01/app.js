73000e56e505fbc204ecb67ef663d86f60fb8b89
Mario Silla Olmos <silolm.mail@gmail.com>
2019-11-02 21:51:01 +0100

    pasarPantalla OK , matar momia OK , quitar vida OK , añadir momia (siguiente Nivel) OK , comentar código OK

--- primeraEvaluacion/UD02/tarea_OhMummy_EP01/app.js
+++ primeraEvaluacion/UD02/tarea_OhMummy_EP01/app.js
@@ -3,18 +3,24 @@ let celdas = [];
 let personaje = {
     y: 0,
     x: 8,
-    vidas: 5
+    salud: 5
 };
 let pergamino = false;
 let llave = false;
 let urna = false;
+// let enemigo = {
+//     y: 0,
+//     x: 8,
+//     temporizador: undefined
+// };
 
-let enemigo = {
-    y: 0,
-    x: 8,
-    temporizador: undefined
-};
+class Enemigo {
+    x = 0;
+    y = 0;
+    temporizador = undefined
+}
 
+let enemigos = [];
 
 function crearMapa() {
     let juego = document.querySelector('.juego');
@@ -45,9 +51,6 @@ function crearMapa() {
             else
                 div.classList.add('celda');
 
-            // if (i === 1 && j === 6)
-            // div.classList.add('enemigo');
-
             mapa[i][j] = div;
             juego.appendChild(div);
         }
@@ -57,6 +60,14 @@ function crearMapa() {
 }
 
 crearMapa();
+
+function pasarPantalla() {
+    document.querySelector('.juego').innerHTML = '';
+    crearMapa();
+    urna = false;
+    llave = false;
+    pergamino = false;
+}
 
 function marcarCeldas() {
     let posY = personaje.y;
@@ -140,16 +151,32 @@ function comprobarCeldas(celda) {
 
 function desbloquearCelda(celda) {
     celdas[celda.x / 4][(celda.y - 1) / 3].estaDescubierta = true;
-    for (let i = 1; i < 4; i++)
-        for (let j = 1; j < 3; j++)
+
+    for (let i = 1; i < 4; i++) {
+        for (let j = 1; j < 3; j++) {
             mapa[celda.y + j][celda.x + i].classList.add(celda.tipo);
+        }
+    }
+    switch (celda.tipo) {
+        case 'urna':
+            urna = true;
+            break;
+        case 'pergamino':
+            pergamino = true;
+            break;
+        case 'llave':
+            llave = true;
+            break;
+    }
+
 }
 
 function llamada(posY, posX) {
+    // ********************* el 1er IF se cumple cuando el personaje se encuentra en un cruce de caminos *********************
     if ((posX === 0 || posX === 20 || posX % 4 === 0) && (posY === 1 || posY === 13 || (posY - 1) % 3 === 0)) {
-        if ((posX === 0 || posX === 20) && (posY === 1 || posY === 13)) {
-            comprobarCeldas(celdas[posX / 5][(posY - 1) / 4])
-        } else {
+        if ((posX === 0 || posX === 20) && (posY === 1 || posY === 13))
+            comprobarCeldas(celdas[posX / 5][(posY - 1) / 4]);
+        else {
             if (posX === 0 || posX === 20) {
                 comprobarCeldas(celdas[posX / 5][(posY - 4) / 3]);
                 comprobarCeldas(celdas[posX / 5][(posY - 1) / 3]);
@@ -163,11 +190,21 @@ function llamada(posY, posX) {
                 comprobarCeldas(celdas[(posX - 4) / 4][(posY - 1) / 3]);
             }
         }
-    } else if (posY === 1 || posY === 13) {
-        comprobarCeldas(celdas[(posX / 5).toFixed(0)][(posY - 1) / 4])
-    } else if (posX === 0 || posX === 20) {
-        comprobarCeldas(celdas[posX / 5][((posY - 1) / 4).toFixed(0)])
-    } else if (posY !== 0) {
+    }
+    //******************************************************************************************************************************
+
+    // Este se cumple está en límite de Y CUANDO NO ES esquina o intersección, es decir cruce de caminos...
+    else if (posY === 1 || posY === 13)
+        comprobarCeldas(celdas[(posX / 5).toFixed(0)][(posY - 1) / 4]);
+    //******************************************************************************************************************************
+
+    // Es el mismo que el de arriba pero con los límetes de X...
+    else if (posX === 0 || posX === 20)
+        comprobarCeldas(celdas[posX / 5][((posY - 1) / 4).toFixed(0)]);
+    //******************************************************************************************************************************
+
+    // Este se ejecuta cuando se encuentra EN LOS LATERALES de las CELDAS....
+    else if (posY !== 0) {
         if (posX % 4 !== 0) {
             comprobarCeldas(celdas[Math.trunc(posX / 4)][Math.trunc((posY - 1) / 3)]);
             comprobarCeldas(celdas[Math.trunc(posX / 4)][Math.trunc((posY - 4) / 3)]);
@@ -176,9 +213,12 @@ function llamada(posY, posX) {
             comprobarCeldas(celdas[Math.trunc((posX - 4) / 4)][Math.trunc((posY - 1) / 3)]);
         }
     }
+    //******************************************************************************************************************************
 }
 
 function crearMomia() {
+    let enemigo = new Enemigo;
+
     var newX = 0;
     var newY = 0;
     while (!mapa[newY][newX].classList.contains("pasillo")) {
@@ -189,48 +229,72 @@ function crearMomia() {
 
     enemigo.x = newX;
     enemigo.y = newY;
-    enemigo.temporizador = setInterval(moverMomia, 300);
+    enemigo.temporizador = setInterval(() => moverMomia(enemigo), 300);
+
+    enemigos.push(enemigo)
 }
 
-function moverMomia() {
+function moverMomia(enemigo) {
 
     if (enemigo.y < 13 && !mapa[enemigo.y + 1][enemigo.x].classList.contains('celda') && enemigo.y < personaje.y) {
-        borrarRastroMomia();
+        borrarRastroMomia(enemigo);
         enemigo.y++;
         mapa[enemigo.y][enemigo.x].classList.add("enemigo");
     }
     if (enemigo.x < 20 && !mapa[enemigo.y][enemigo.x + 1].classList.contains('celda') && enemigo.x < personaje.x) {
-        borrarRastroMomia();
+        borrarRastroMomia(enemigo);
         enemigo.x++;
         mapa[enemigo.y][enemigo.x].classList.add("enemigo");
     }
-    if (enemigo.y > 0 && !mapa[enemigo.y - 1][enemigo.x].classList.contains('celda') && enemigo.y > personaje.y) {
-        borrarRastroMomia();
+    if ((enemigo.y > 1 || (enemigo.x === 8 && enemigo.y > 0)) && !mapa[enemigo.y - 1][enemigo.x].classList.contains('celda') && enemigo.y > personaje.y) {
+        borrarRastroMomia(enemigo);
         enemigo.y--;
         mapa[enemigo.y][enemigo.x].classList.add("enemigo");
     }
     if (enemigo.x > 0 && !mapa[enemigo.y][enemigo.x - 1].classList.contains('celda') && enemigo.x > personaje.x) {
-        borrarRastroMomia();
+        borrarRastroMomia(enemigo);
         enemigo.x--;
         mapa[enemigo.y][enemigo.x].classList.add("enemigo");
     }
 
+    cruceDeCaminos(enemigo);
 }
 
-function borrarRastroMomia() {
+function borrarRastroMomia(enemigo) {
     mapa[enemigo.y][enemigo.x].classList.remove("enemigo");
+}
+
+function cruceDeCaminos(enemigo) {
+    if (enemigo === undefined) {
+        enemigos.filter(ele => ele.x === personaje.x && ele.y === personaje.y).forEach(enemigo => {
+            matarMomia(enemigo);
+        });
+    }
+
+    else if (enemigo.x === personaje.x && enemigo.y === personaje.y)
+        matarMomia(enemigo);
 }
 
 function matarMomia() {
     if (!pergamino) quitarVida();
     else pergamino = false;
-    
-    clearInterval(enemigo.temporizador);
-    borrarRastroMomia();
+
+    enemigos.filter(element => element.x === personaje.x && element.y === personaje.y).forEach(enemigo => {
+        clearInterval(enemigo.temporizador);
+        borrarRastroMomia(enemigo);
+    });
+
+    enemigos = enemigos.filter(element => !(element.x === personaje.x && element.y === personaje.y))
 }
 
 function quitarVida() {
-    
+    personaje.salud--;
+    console.log(personaje.salud);
+
+    if (personaje.salud < 1) {
+        // GAME OVER
+    }
+
 }
 
 // Movimientos
@@ -275,8 +339,8 @@ function moverIzquierda() {
 }
 
 function mover(posY, posX) {
-    console.log(posX);
-    console.log(posY);
+    // console.log(posX);
+    // console.log(posY);
 
     // Eliminar anterior posicion
     mapa[personaje.y][personaje.x].classList.remove('personaje', 'personajeIzquierda', 'personajeAbajo');
@@ -286,6 +350,10 @@ function mover(posY, posX) {
     personaje.y = posY;
 
     marcarCeldas();
+    cruceDeCaminos();
+
+    if ((personaje.x === 8 && personaje.y === 0) && urna && llave)
+        pasarPantalla();
 }
 
 function esValido(y, x) {
