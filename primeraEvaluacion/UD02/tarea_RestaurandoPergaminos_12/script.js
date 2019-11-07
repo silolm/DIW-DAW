6be7c987b1b7fdf00f3f5807642967d3ded317c8
Mario Silla Olmos <silolm.mail@gmail.com>
2019-11-07 20:40:51 +0100

    mover rojo

--- primeraEvaluacion/UD02/tarea_RestaurandoPergaminos_12/script.js
+++ primeraEvaluacion/UD02/tarea_RestaurandoPergaminos_12/script.js
@@ -1,7 +1,25 @@
-/* 
- 
-    ^(;,;)^ : Fragmento perdido
+window.onload = init;
+let contador = 0;
 
-*/
+function crearCajas() {
+    if (contador <= 17) {
+        let padre = document.querySelector('container');
+        let caja = document.createElement('box');
 
-window.onload=init;
+        caja.addEventListener('click', click);
+
+        padre.appendChild(caja);
+        contador++;
+    }
+}
+
+function click() {
+
+    if (!this.classList.contains('evoluciona'))
+        this.classList.add('evoluciona');
+
+}
+
+function init() {
+    document.querySelector('button').addEventListener('click', crearCajas);
+}
