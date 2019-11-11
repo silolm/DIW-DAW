124494830970e7654cab133837d1a9576c7b5bd3
Mario Silla Olmos <silolm.mail@gmail.com>
2019-11-11 13:20:32 +0100

    a

--- primeraEvaluacion/UD02/tareaLosTamboresDeYasuo_09/drumkit.js
+++ primeraEvaluacion/UD02/tareaLosTamboresDeYasuo_09/drumkit.js
@@ -1,26 +1,18 @@
-window.onload=init;
+window.onload = init;
 
-
-function suena(e){
+function suena(e) {
     let audio;
     audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-    if (audio == null)return;
+    if (audio == null) return;
 
     const nuestroDiv = document.querySelector(`keys[data-key="${e.keyCode}"]`);
     nuestroDiv.classList.add("key");
-    audio.currentTime=0;
+    audio.currentTime = 0;
     audio.play();
-
-
 }
 
 
-function init(){
-    window.addEventListener("keydown",suena);
+function init() {
+    window.addEventListener("keydown", suena);
 }
-
-/*
-window.addEventListener('keydown', () => {
-
-});**/
