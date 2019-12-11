c80bc4cd047b4cdaa539762863ed32c28eea2916
Mario Silla Olmos <silolm.mail@gmail.com>
2019-12-11 17:43:07 +0100

    Added some useful scripts

--- segundaEvaluacion/UD04/tarea_01_NodeJSPlusSass/server.js
+++ segundaEvaluacion/UD04/tarea_01_NodeJSPlusSass/server.js
@@ -2,10 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Importamos MongoDB
+const MongoClient = require('mongodb').MongoClient;
+const mongoUrl = "mongodb://localhost/mongoFallero";
+
 // Paginas publicas (estaticas)
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Escuchemos en un puerto
 app.listen(3000, () => {
-    console.log(" * Miniserver UP and Running en http://localhost:3000");
+    console.log(" * [ Mongo Fallero ] UP and Running en http://localhost:3000");
 });
