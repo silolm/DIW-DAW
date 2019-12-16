69b5e6b9c82fe149c351f4a76e39db96d17a772c
silolm <55095616+silolm@users.noreply.github.com>
2019-12-16 13:30:09 +0100

    fallas

--- segundaEvaluacion/UD04/tarea_01_NodeJSPlusSass/public/js/app.js
+++ segundaEvaluacion/UD04/tarea_01_NodeJSPlusSass/public/js/app.js
@@ -1,65 +1,66 @@
-const fuentesUrl = "http://mapas.valencia.es/lanzadera/opendata/Monumentos_falleros/JSON";
+const fallasUrl = 'http://mapas.valencia.es/lanzadera/opendata/Monumentos_falleros/JSON';
+let busquedaDeFallas;
 
-// Esta es la funcion de filtrado para
-// obtener tan solo los elementos que cumplen
-// una serie de requisitos.
 
-function filtroLetra(elemento) {
-    let letra = document.querySelector(`input[name="calle"]`).value;
-    return elemento.properties.calle.startsWith(letra);
+function secciones(){    
+    let opciones = document.querySelector('select');
+
+    // Set -> ArrayList que sólo nos añade los valores que no se repitan dentro de un array;
+    let seccion = new Set;
+
+    // Primer valor del Select [Sección] -> TODAS
+    seccion.add('Todas');
+
+    seccion.array.forEach(option => {
+        let list;
+        
+        // Obtener un 'option' por cada sección del JSON
+        list = document.createElement('option');
+        
+        // Le añadimos su valor;
+        list.innerText = option;
+
+        // Le decimos a quien pertenece;
+        opciones.appendChild(list);
+    });
+
+
+    // Alojamos la lista de Fallas del JSON en -> featureSearch;
+    let featureSearch = busquedaDeFallas.features;
+
+    featureSearch.array.forEach(element => {
+        seccion.add(element.propierties.seccion);
+    });
+
+    
+    document.querySelector('label[for="selector"]').insertBefore(opciones, document.querySelector('form'));
 }
-
-// Pasa a mayuscula el texto de propio input
-// se lanza cada vez que se realiza una insercion en
-// el texto del nombre.
-function toUpp() {
-    document.querySelector(`input[name="calle"]`).value = document.querySelector(`input[name="calle"]`).value.toUpperCase();
-}
-
 
 function buscar() {
+   
+}
 
-    // Obtenemos el JSON que esta definido
-    const fetchPromesa = fetch(fuentesUrl);
-
-    // Cuando se resuelva la promesa
-    fetchPromesa.then(response => {
+function safeData() {
+    fetch(fallasUrl).then(response => {
         // la pasamos a JSON
         return response.json();
         // Y entonces
-    }).then(respuesta => {
-        // Filtramos los resultados con el filtro definido anteriormente
-
-        console.log(respuesta.features[1].properties);
-        const resultado = respuesta.features.filter(filtroLetra);
-
-        // Una vez tenemos el listado filtrado pasamos a crear
-        // cada uno de los <li> que representan
-        let listado = document.createElement("ul");
-
-        // Por cada uno de ellos 
-        resultado.forEach(fuente => {
-            // Creamos un <li>
-            let calleli = document.createElement("li");
-            calleli.innerHTML = fuente.properties.calle + " -- [" + fuente.geometry.coordinates + "]";
-            // Lo anyadimos
-            listado.appendChild(calleli);
-        });
-        // Establecemos el listado en la Web
-        document.querySelector(".resultados").innerHTML = "";
-        document.querySelector(".resultados").appendChild(listado);
+    }).then(busqueda => {
+        console.log(busqueda);
+        busquedaDeFallas = busqueda;
     });
 }
 
-function init() {
-
-    // Binding de los eventos correspondientes.
-
-    // Click en el boton de buscar
-    document.querySelector(`input[type="button"]`).addEventListener("click", buscar);
-    // Texto cambia en el <input>
-    document.querySelector(`input[type="text"]`).addEventListener("input", toUpp);
+async function init() {
+    await safeData();
+    secciones();
+    
+    
+    document.querySelector('select').addEventListener('change', buscar);
+    document.getElementById('desde').addEventListener('change', buscar);
+    document.getElementById('hasta').addEventListener('change', buscar);
+    document.getElementById('principal').addEventListener('click', buscar);
+    document.getElementById('infantil').addEventListener('click', buscar);
 }
 
-// The mother of the lamb.
 window.onload = init;
