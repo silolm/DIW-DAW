0b3c56ae4a68e5563ee7f10f140de27c4f154378
Mario Silla Olmos <silolm.mail@gmail.com>
2019-12-24 20:28:12 +0100

    reorganizar codigo, funcion buscar filtra por AÑO, Seccion y Categoria (Principal o infantil);

--- segundaEvaluacion/UD04/tarea_01_NodeJSPlusSass/public/js/app.js
+++ segundaEvaluacion/UD04/tarea_01_NodeJSPlusSass/public/js/app.js
@@ -1,5 +1,6 @@
 const fallasUrl = 'http://mapas.valencia.es/lanzadera/opendata/Monumentos_falleros/JSON';
 let busquedaDeFallas;
+let busquedaActual;
 
 function secciones() {
     let select = document.querySelector('select');
@@ -10,11 +11,9 @@ function secciones() {
     seccion.add('Todas');
 
     // Alojamos la lista de Fallas del JSON en -> featureSearch;
-    let featureSearch = busquedaDeFallas.features;
-
-    featureSearch.forEach(element => {
+    busquedaDeFallas.forEach(element => {
         // Obtener un 'option' por cada sección del JSON
-        seccion.add(element.properties.seccion);
+        seccion.add(element.seccion);
     });
 
     seccion.forEach(element => {
@@ -27,21 +26,63 @@ function secciones() {
 }
 
 function buscar() {
+    let selector = document.getElementById('selector').value;
+    let principal = document.getElementById('principal').checked;
+    let infantil = document.getElementById('infantil').checked;
+
+    let desde = document.getElementById('desde').value;
+    if (desde === '') desde = 0;
+
+    let hasta = document.getElementById('hasta').value;
+    if (hasta === '') hasta = 3000;
+
+    busquedaActual = busquedaDeFallas.filter((element) => {
+        if (selector === 'Todas' || selector === element.seccion) {
+            if (infantil === element.infantil || (!principal && !infantil)) {
+                if (element.anyo >= desde && element.anyo <= hasta) {
+                    return true;
+                }
+            }
+        }
+        return false
+    });
+
+    cargarBocetos();
+}
+
+function cargarBocetos() {
+
 
 }
 
-function safeData() {
+function saveData() {
     return fetch(fallasUrl).then(response => {
         // la pasamos a JSON
         return response.json();
         // Y entonces
     }).then(busqueda => {
-        busquedaDeFallas = busqueda;
+        busquedaDeFallas = busqueda.features.reduce((buffer, element) => buffer.concat([
+            {
+                nombre: element.properties.nombre,
+                seccion: element.properties.seccion,
+                anyo: element.properties.anyo_fundacion,
+                boceto: element.properties.boceto,
+                infantil: false
+            },
+            {
+                nombre: element.properties.nombre,
+                seccion: element.properties.seccion_i,
+                anyo: element.properties.anyo_fundacion_i,
+                boceto: element.properties.boceto_i,
+                infantil: true
+            }]), []);
+
+        busquedaActual = busquedaDeFallas;
     });
 }
 
 async function init() {
-    await safeData();
+    await saveData();
 
     secciones();
 
