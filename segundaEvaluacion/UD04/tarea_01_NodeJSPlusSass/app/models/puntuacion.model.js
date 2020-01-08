708a6e42b7a7d9bd1670aa7390068ff677e1cd4b
Mario Silla Olmos <silolm.mail@gmail.com>
2020-01-08 16:55:11 +0100

    puntuaciones en progreso, añadido => M V C , funcion puntuar en progreso...

---
+++ segundaEvaluacion/UD04/tarea_01_NodeJSPlusSass/app/models/puntuacion.model.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const PuntuacionSchema = mongoose.Schema({
+    idFalla: String,
+    ip: String,
+    puntuacion: Number
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Puntuacion', PuntuacionSchema);
