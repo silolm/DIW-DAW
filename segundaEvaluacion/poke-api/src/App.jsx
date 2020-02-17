be51c63a7b9a80dbda7ae518de20861572ff526d
Mario Silla Olmos <silolm.mail@gmail.com>
2020-02-17 16:36:07 +0100

    add plguins, libs and restructurate folders

--- segundaEvaluacion/poke-api/src/App.jsx
+++ segundaEvaluacion/poke-api/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-            Hola Mundo
+          Hola Mundo
         </p>
         <a
           className="App-link"
