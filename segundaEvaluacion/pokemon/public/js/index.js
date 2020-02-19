ae5a2486f03133f6eba1e4fd82933bea3ff89198
Mario Silla Olmos <silolm.mail@gmail.com>
2020-02-19 16:47:47 +0100

    arreglos de pokemon

--- segundaEvaluacion/pokemon/public/js/index.js
+++ segundaEvaluacion/pokemon/public/js/index.js
@@ -2,34 +2,36 @@ const poke_list = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=964';
 let filter;
 let currentFilter;
 
-async function consulta(element) {
-    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${element}`);
+async function savePokemon() {
+    const response = await fetch(poke_list);
     const search = await response.json();
-    filter = search.forms.map(element => ({
+    filter = search.results.map(element => ({
         name: element.name,
         url: element.url
     }));
     currentFilter = filter;
+}
 
+function consulta() {
     mostrar();
 
     function mostrar() {
         let padre;
-
         padre = document.getElementById('resultados');
         padre.innerHTML = '';
 
-        for (let i = 0; i < 5; i++) {
+        for (let i = 0; i < currentFilter.length; i++) {
             let hijo = document.createElement('a');
-            hijo.innerText = currentFilter.name;
+            hijo.innerText = currentFilter[i].name;
             padre.appendChild(hijo);
         }
     }
 }
 
-function init() {
-    let valorBusqueda = document.querySelector('#myInput');
+async function init() {
+    await savePokemon();
 
+    let valorBusqueda = document.querySelector('#myInput');
     document.querySelector('#myInput').addEventListener('keyup', () => {
         consulta(valorBusqueda.value);
     });
