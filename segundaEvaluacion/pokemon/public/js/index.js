53d0bb4d3303e6e95d4c8e685ed0cb11ef530e5b
Mario Silla Olmos <silolm.mail@gmail.com>
2020-02-24 14:43:19 +0100

    ultimos retoques

--- segundaEvaluacion/pokemon/public/js/index.js
+++ segundaEvaluacion/pokemon/public/js/index.js
@@ -1,76 +1,87 @@
-const poke_list = 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20';
-let pokeFilter;
-let currentFilter;
-let pokeImgFilter;
-let currentImgFilter;
+let urlPokemons;
+let pokemons;
 
-async function savePokemon() {
-    const pokeList = await fetch(poke_list);
-    const pokeSearch = await pokeList.json();
-    pokeFilter = pokeSearch.results.map(element => ({
-        name: element.name,
-        url: element.url
-    }));
-    currentFilter = pokeFilter;
-    console.log(currentFilter);
+async function saveUrl() {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20`);
+    const json = await response.json();
+    return urlPokemons = json.results;
 }
 
-async function savePokeImg(pokemon) {
-    const pokemonImg = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
-    const imgSearch = await pokemonImg.json();
-    console.log(imgSearch);
-    pokeImgFilter = imgSearch.sprites.map(element => ({
-        img: element.front_default
+async function savePokemons() {
+    pokemons = await Promise.all(urlPokemons.map(async element => {
+        const response = await fetch(element.url);
+        const json = await response.json();
+        return {
+            name: json.name,
+            moves: {
+                move1: json.moves[0].move.name,
+                move2: json.moves[1].move.name,
+                move3: json.moves[2].move.name,
+                move4: json.moves[3].move.name,
+            },
+            types: json.types[0].type.name,
+            weight: json.weight,
+            height: json.height,
+            img: json.sprites.front_default
+        };
     }));
-    currentImgFilter = pokeImgFilter;
+    //console.log(pokemons);
 }
 
-function cargarPokemons() {
+function loadPokemons() {
     let contenedor = document.getElementById('pokeResults');
     contenedor.innerHTML = '';
-
-    currentFilter.forEach((element) => {
-        contenedor.appendChild(cargarPokemon(element));
+    pokemons.forEach((element) => {
+        contenedor.appendChild(loadInfo(element));
     });
 }
 
+function loadInfo(pokemon) {
+    let container = document.querySelector('#pokeResults');
+    //document.querySelector('body').style.overflow = 'hidden';
 
-function cargarPokemon(pokemon) {
     let caja = document.createElement('div');
-    caja.classList.add('cajaPokemon');
-    caja.dataset.name = pokemon.name;
+    caja.id = 'pokedexBox';
 
-    caja.innerHTML = `
-        <h3>${pokemon.name}</h3>
+    let pokedex = document.createElement('div');
+    pokedex.classList.add('pokedex');
+
+    pokedex.innerHTML = `
+    <section class="wrapper">
+        <!-- Row title -->
+        <main class="row title">
+            <ul>
+                <li>Name</li>
+                <li>Types</li>
+                <li><span class="title-hide">#</span>Moves</li>
+                <li>Height</li>
+                <li>Weight</li>
+            </ul>
+        </main>
+        <!-- Row 1 - fadeIn -->
+        <section class="row-fadeIn-wrapper">
+            <article class="row fadeIn nfl">
+                <ul>
+                    <li><a>${pokemon.name}</a></li>
+                    <li>TYPE</li>
+                    <li>MOVES</li>
+                    <li>${pokemon.height}</li>
+                    <li>${pokemon.weight}</li>
+                </ul>
+            </article>
+        </section>
+    </section>
     `;
+    container.appendChild(caja);
+    caja.appendChild(pokedex);
 
     return caja;
 }
 
-function consulta() {
-    mostrar();
-
-    function mostrar() {
-        let padre;
-        padre = document.getElementById('resultados');
-        padre.innerHTML = '';
-
-        for (let i = 0; i < currentFilter.length; i++) {
-            let hijo = document.createElement('a');
-            hijo.innerText = currentFilter[i].name;
-            padre.appendChild(hijo);
-        }
-    }
-}
-
 async function init() {
-    await savePokemon();
-    cargarPokemons();
-
-    let valorBusqueda = document.querySelector('#myInput');
-    document.querySelector('#myInput').addEventListener('keyup', () => {
-        consulta(valorBusqueda.value);
-    });
+    await saveUrl();
+    await savePokemons();
+    loadPokemons();
 }
 
 window.onload = init;
